2d248f4247e5798f0e0618fc2b6bc6cdd5bce962
wwf <fjgc_wwf@126.com>
2019-04-11 16:38:27 +0800

    初始化

---
+++ src/components/head.vue
@@ -0,0 +1,35 @@
+<template>
+  <el-container>
+    <el-header style="text-align: right; font-size: 12px">
+      <el-menu
+        :default-active="activeIndex"
+        class="el-menu-demo"
+        mode="horizontal"
+        @select="handleSelect"
+      >
+        <el-menu-item index="1">处理中心</el-menu-item>
+        <el-menu-item index="2">处理中心</el-menu-item>
+        <el-menu-item index="3">处理中心</el-menu-item>
+      </el-menu>
+    </el-header>
+
+    <el-main>
+      <el-table :data="tableData">
+        <el-table-column prop="date" label="日期" width="140">
+        </el-table-column>
+        <el-table-column prop="name" label="姓名" width="120">
+        </el-table-column>
+        <el-table-column prop="address" label="地址"> </el-table-column>
+      </el-table>
+    </el-main>
+  </el-container>
+</template>
+
+<script>
+export default {
+  name: "head",
+  props: {
+    msg: String
+  }
+};
+</script>
---
+++ src/views/index.vue
@@ -0,0 +1,105 @@
+<template>
+  <el-container>
+    <el-header style="text-align: right; font-size: 12px">
+      <div class="navbar">
+        <div class="right-menu">
+          <el-menu
+            :default-active="activeIndex"
+            class="el-menu-demo"
+            mode="horizontal"
+            @select="handleSelect"
+            background-color="#545c64"
+            text-color="#fff"
+            active-text-color="#ffd04b"
+          >
+            <el-menu-item index="1">处理中心</el-menu-item>
+            <el-menu-item index="2">处理中心</el-menu-item>
+            <el-menu-item index="3">处理中心</el-menu-item>
+            <el-menu-item index="4">处理中心</el-menu-item>
+            <el-menu-item index="5">处理中心</el-menu-item>
+          </el-menu>
+        </div>
+      </div>
+    </el-header>
+
+    <el-main>
+      <el-table :data="tableData">
+        <el-table-column prop="date" label="日期" width="140">
+        </el-table-column>
+        <el-table-column prop="name" label="姓名" width="120">
+        </el-table-column>
+        <el-table-column prop="address" label="地址"> </el-table-column>
+      </el-table>
+    </el-main>
+  </el-container>
+</template>
+
+<script>
+import helloword from "../components/HelloWorld.vue";
+export default {
+  name: "Layout",
+  components: { helloword }
+};
+</script>
+
+<style lang="scss" scoped>
+.navbar {
+  height: 50px;
+  overflow: hidden;
+  position: relative;
+  background: #545c64;
+  box-shadow: 0 1px 4px rgba(0, 21, 41, 0.08);
+
+  .right-menu {
+    float: right;
+    height: 100%;
+    line-height: 50px;
+
+    &:focus {
+      outline: none;
+    }
+
+    .right-menu-item {
+      display: inline-block;
+      padding: 0 8px;
+      height: 100%;
+      font-size: 18px;
+      color: #5a5e66;
+      vertical-align: text-bottom;
+
+      &.hover-effect {
+        cursor: pointer;
+        transition: background 0.3s;
+
+        &:hover {
+          background: rgba(0, 0, 0, 0.025);
+        }
+      }
+    }
+
+    .avatar-container {
+      margin-right: 30px;
+
+      .avatar-wrapper {
+        margin-top: 5px;
+        position: relative;
+
+        .user-avatar {
+          cursor: pointer;
+          width: 40px;
+          height: 40px;
+          border-radius: 10px;
+        }
+
+        .el-icon-caret-bottom {
+          cursor: pointer;
+          position: absolute;
+          right: -20px;
+          top: 25px;
+          font-size: 12px;
+        }
+      }
+    }
+  }
+}
+</style>
